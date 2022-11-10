581f41db6b3338b278e029187d98ffa0fa2e7326
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-10 11:48:50 +0300

    05- generate email

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,9 +7,11 @@ public class Email  {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private int mailboxCapacity;
     private int defaultPasswordLength = 8;
     private String alternateEmail;
+    private String companySuffix = "nekcompany.com";
 
     //Constructor: to receive the first and last name
 
@@ -25,6 +27,10 @@ public class Email  {
         //Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is : " + this.password);
+
+        //Combine elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "."+ companySuffix;
+        System.out.println("Your email is : " + email);
     }
 
 

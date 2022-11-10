c54837e1534d3e23a1c8b21c8f39bb9f21376eea
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-10 11:12:57 +0300

    03 - asking for the department by setDepartment method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email  {
     private String firstName;
     private String lastName;
@@ -10,7 +12,33 @@ public class Email  {
 
     //Constructor: to receive the first and last name
 
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED "  + this.firstName+ " " +this.lastName);
+
+        //Call a method asking for the department - and return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+
     //Ask for the department
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code : ");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
+        if(departmentChoice == 1){
+            return "sales";
+        } else if (departmentChoice == 2) {
+            return "dev";
+
+        } else if (departmentChoice == 3) {
+            return "acct";
+        }else {
+            return "";
+        }
+    }
 
     //Generate a random password
 
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -2,6 +2,6 @@ package emailapp;
 
 public class EmailApp {
     public static void main(String[] args) {
-
+        Email email = new Email("Ahmet", "Özdemir");
     }
 }

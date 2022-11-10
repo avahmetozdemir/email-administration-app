bd5a33f211554694f6030d39920aff9a4c1fb6c2
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-10 11:00:59 +0300

    02- add email class with properites and steps to create the class and emailapp class with main

---
+++ src/emailapp/Email.java
@@ -0,0 +1,26 @@
+package emailapp;
+
+public class Email  {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor: to receive the first and last name
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+
+    //Change the password
+
+
+
+}
